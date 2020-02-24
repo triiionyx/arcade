ff9ffa7a63e5cd056df84a4724d99710ff939132
Tim Banon <izmbanon@gmail.com>
2020-02-23 22:08:48 -0500

    Updated framework

--- www/cdn/js/router.js
+++ www/cdn/js/router.js
@@ -1,5 +1,5 @@
 String.prototype.router = function(b, state = this.valueOf()) { //window.location.href = state;
   return new Promise(function(resolve, reject) {
-    history.pushState(state,null,state)
+    history.pushState(state,null,state);
   });
 };
