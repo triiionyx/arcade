8f4003590cc51900e8185859cba343953f59d305
Tim Banon <izmbanon@gmail.com>
2020-02-23 22:13:12 -0500

    Updated filesystem

--- www/cdn/js/router.js
+++
@@ -1,5 +0,0 @@
-String.prototype.router = function(b, state = this.valueOf()) { //window.location.href = state;
-  return new Promise(function(resolve, reject) {
-    history.pushState(state,null,state);
-  });
-};
